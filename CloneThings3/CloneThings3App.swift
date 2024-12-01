2faf53110612f83542ffc5780b483c04486fd40c
AkaCoder404 <akacoder404@outlook.com>
2024-12-01 13:07:07 +0800

    Initial Commit

---
+++ CloneThings3/CloneThings3App.swift
@@ -0,0 +1,20 @@
+//
+//  CloneThings3App.swift
+//  CloneThings3
+//
+//  Created by George Li on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneThings3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
